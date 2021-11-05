9d2d3d0b632a23ab7f185bd0121e0ae8160bd6b6
Alex Novak <alexleonnovak@gmail.com>
2021-11-05 21:49:47 +0200

    fix get all wallets

--- controllers/wallet/getWallets.js
+++ controllers/wallet/getWallets.js
@@ -2,7 +2,7 @@ const WalletRepo = require("../../repositories/wallet");
 
 const getWallets = async (req, res) => {
   const wallets = await WalletRepo.getWallets(req.user.id, req.query);
-  return res.OK({ ...wallets });
+  return res.OK({ wallets });
 };
 
 module.exports = getWallets;
